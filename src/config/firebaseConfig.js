528c9d1dc675d1c17e58684b1e10a1b84c0a979f
aman shrivastava <aman34503@gmail.com>
2023-01-30 02:21:12 +0530

    update changes

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUbfe9nDN0Me40PnvqDJHSDj02KlZzAmQ",
+  authDomain: "fasal-2762a.firebaseapp.com",
+  projectId: "fasal-2762a",
+  storageBucket: "fasal-2762a.appspot.com",
+  messagingSenderId: "1005883657395",
+  appId: "1:1005883657395:web:40eec68f0a14df94e0f4c6",
+};
+
+export default firebaseConfig;
